d39322d57dc696051e7fa37f4cda36cc60c1e4fe
FREEDOM <freewhynane62@gmail.com>
2021-07-28 11:58:51 +0000

    add pages on router

--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from '../Pages/Home/Home';
+import Login from '../Pages/Login/Login';
+import Register from '../Pages/Register/Register';
+import Startups from '../Pages/Startups/Startups';
 
 const Router = () => {
     return (
         <BrowserRouter>
-            
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/startups" component={Startups} />
+                <Route exact path="/register" component={Register} />
+                
+            </Switch>
         </BrowserRouter>
     );
 }
