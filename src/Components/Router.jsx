f41cb7f88e46db58341ab3bcd01ea03b8c8f3f61
FREEDOM <freewhynane62@gmail.com>
2021-07-28 09:23:57 +0000

    remove unuse images

--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 const Router = () => {
     return (
-        <div>
+        <BrowserRouter>
             
-        </div>
+        </BrowserRouter>
     );
 }
 
